7c55af38eceae5bafd70af4841df994cd0c07d7b
김종성 <jseongee@gmail.com>
2025-07-02 17:55:19 +0900

    feat: Add profile area in home tab

--- banking/Views/HomeViewController.swift
+++ banking/Views/HomeViewController.swift
@@ -1,7 +1,34 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+
+    private var user: User?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        loadUserData()
+        setupProfileUI()
+        updateProfileData()
+    }
+
+    private func loadUserData() {
+        user = SampleDataGenerator.createSampleUser()
+    }
+
+    private func setupProfileUI() {
+        profileImageView.layer.cornerRadius = profileImageView.layer.frame.width / 2
+    }
+
+    private func updateProfileData() {
+        guard let user else {
+            print("유저 데이터가 없습니다.")
+            return
+        }
+
+        profileImageView.image = UIImage(systemName: user.profileImage)
+        nameLabel.text = "Hello \(user.name)!"
     }
 }
