f51a99c4f29551327a1e426a374dbdcf620eac63
김종성 <jseongee@gmail.com>
2025-07-09 23:35:25 +0900

    feat: Add activity more menu

--- banking/Views/ActivityViewController.swift
+++ banking/Views/ActivityViewController.swift
@@ -1,8 +1,17 @@
 import UIKit
 
 class ActivityViewController: UIViewController {
+	// MARK: - Outlets
+    @IBOutlet weak var moreButton: UIBarButtonItem!
+
+    // MARK: - Properties
+    private let categories = ["Checking", "Savings", "Crypto"]
+        private var selectedCategoryIndex = 0
+
+    // MARK: - Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupMoreMenu()
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -21,6 +30,25 @@ class ActivityViewController: UIViewController {
         }
     }
 
+    private func setupMoreMenu() {
+        let menu = UIMenu(title: "", children: [
+            UIAction(title: "Export Data", image: UIImage(systemName: "square.and.arrow.up")) { _ in
+                print("Export Data tapped")
+            },
+            UIAction(title: "Account Settings", image: UIImage(systemName: "gear")) { _ in
+                print("Account Settings tapped")
+            },
+            UIAction(title: "Help & Support", image: UIImage(systemName: "questionmark.circle")) { _ in
+                print("Help & Support tapped")
+            },
+            UIAction(title: "Privacy Policy", image: UIImage(systemName: "hand.raised")) { _ in
+                print("Privacy Policy tapped")
+            }
+        ])
+
+        moreButton.menu = menu
+    }
+
     private func findCustomTabBarController() -> CustomTabBarController? {
         var parentVC = self.parent
         while parentVC != nil {
@@ -32,7 +60,8 @@ class ActivityViewController: UIViewController {
         return nil
     }
 
-    @IBAction func closeVC(_ sender: Any) {
+    // MARK: - Actions
+    @IBAction func backButtonTapped(_ sender: UIBarButtonItem) {
         navigationController?.popViewController(animated: true)
     }
 }
